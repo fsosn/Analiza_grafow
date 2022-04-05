a4ce8581cd95baf4cb46a5d5d203853ad07b7b6a
Filip Sosnowski <filip.srebrna@gmail.com>
2022-04-06 00:55:32 +0200

    Dodanie zapisu do pliku

---
+++ to_file.c
@@ -0,0 +1,37 @@
+#include "to_file.h"
+
+#include <stdio.h>
+#include <stdlib.h>
+
+void to_file(int x, int y, double arr[x*y][x*y], char* output)
+{
+	FILE* out = fopen(output, "w");
+	
+	if( out == NULL )
+	{
+		fprintf(stderr, "%s", "Nie udalo sie otworzyć pliku wyjsciowego.\n");
+		exit(EXIT_FAILURE);
+	}
+
+	int i, j;
+	int n = x * y;
+	
+	fprintf(out, "%d %d\n", x, y);
+
+	for(i = 0; i < n; i++)
+	{
+		fprintf(out, "\t");
+
+		for(j = 0; j < n; j++)
+		{
+			if(arr[i][j] != 0)
+			{
+				fprintf(out, "%d: %g ", j, arr[i][j]);
+			}
+		}
+		if( i != n-1 )
+			fprintf(out, "\n");	
+	}
+
+	fclose(out);
+}
