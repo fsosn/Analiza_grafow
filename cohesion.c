cc4d7ef1a506c9788770e52913ca1977ca6bd823
KTSKG <ktskg02@gmail.com>
2022-04-11 22:43:28 +0200

    Cohesion v2

--- cohesion.c
+++ cohesion.c
@@ -11,7 +11,7 @@ void cohesion(int x, int y, double arr[x*y][x*y],int n){
 	
 	int i,j,k;
 	int a=x*y;
-	int edge[a];
+	int (*edge) = calloc(a, sizeof * edge);
 	int rand_start;
 	int rand_dir;
 	
@@ -28,7 +28,7 @@ void cohesion(int x, int y, double arr[x*y][x*y],int n){
 
 	}
 	rand_start = rand() % a;
-
+	
 	while(edge[rand_start]==4){
 		rand_start = rand() % a;
 	}
@@ -40,17 +40,88 @@ void cohesion(int x, int y, double arr[x*y][x*y],int n){
 		}
 		printf("\n");
 	}
+	i = 0;
+	j = 0;
+	//pierwszy ruch po wyzanczeniu punktu
+			if(rand_start % x == 0 && rand_start < 1){	
+				rand_dir = 1;
+				while(i!=1){
+					if(arr[j][rand_start]==1){
+						arr[j][rand_start] = 0;
+						arr[rand_start][j] = 0;
+						i++;
+					}
+					j++;
+				}
+			}
+			if(rand_start < x && rand_start >= 1 ){
+				rand_dir = 2;
+				while(i!=2){
+					if(arr[j][rand_start]==1){
+						arr[j][rand_start] = 0;
+						arr[rand_start][j] = 0;
+						i++;
+					}
+					j++;
+				}
+			}
+			if(rand_start % x == x-1 && rand_start < x){
+				rand_dir = 3;
+				while(i!=1){
+					if(arr[j][rand_start]==1){
+						arr[j][rand_start] = 0;                                                                                                                                                                                                                      arr[rand_start][j] = 0;                                                                                                                                                                                                                      i++;                                                                                                                                                                                                                                 }
+					j++;
+				}
+			}
+			if(rand_start % x == 0 && rand_start >= x && rand_start < x*y-x){
+				rand_dir = 1;
+				while(i!=2){
+					if(arr[j][rand_start]==1){
+						arr[j][rand_start] = 0;                                                                                                                                                                                                                      arr[rand_start][j] = 0;                                                                                                                                                                                                                      i++;                                                                                                                                                                                                                                 }
+					j++;
+				}
+			}
+			if(rand_start % x == x-1 && rand_start > x && rand_start <=x*y-x-1){
+				rand_dir = 3;
+				while(i!=2){
+					if(arr[j][rand_start]==1){
+						arr[j][rand_start] = 0;
+						arr[rand_start][j] = 0;
+						i++;
+					}
+					j++;
+				}
+			}
+			if(rand_start = x*y-x ){
+				rand_dir = 1;
+				while(i!=1){
+					if(arr[j][rand_start]==1){
+						arr[j][rand_start] = 0;
+						arr[rand_start][j] = 0;
+						i++;
+					}
+					j++;
+				}
+			}
+			if(rand_start > x*y-x && rand_start < x*y-1){
+				rand_dir = 0;
+				while(i!=2){
+					if(arr[j][rand_start]==1){
+						arr[j][rand_start] = 0;                                                                                                                                                                                                                      arr[rand_start][j] = 0;                                                                                                                                                                                                                      i++;                                                                                                                                                                                                                                 }
+					j++;
+				}
+			}
+			if(rand_start == x*y-1){
+				rand_dir = 3;
+				while(i!=1){
+					if(arr[j][rand_start]==1){
+						arr[j][rand_start] = 0;                                                                                                                                                                                                                      arr[rand_start][j] = 0;                                                                                                                                                                                                                      i++;                                                                                                                                                                                                                                 }
+					j++;
+				}
+			}
 
-	i=0;
-	j=0;
-	while(i!=2){
-		if(arr[j][rand_start]==1){
-			arr[j][rand_start] = 0;
-			arr[rand_start][j] = 0;
-			i++;
-		}
-		j++;
-	}
+			
+
 
 	printf("\n");
 	for(i=0;i<a;i++){
@@ -60,12 +131,24 @@ void cohesion(int x, int y, double arr[x*y][x*y],int n){
 		printf("\n");
 	}
 	printf("\n%d - rand_start",rand_start);
+
+	if(edge[rand_start]==3){
+		printf("\ne rand_start - 3");
+	}
+        else if(edge[rand_start]==2){
+		printf("\ne rand_start - 2");
+	}
+	else{
+		printf("\ne rand_start - 1");
+	}	
+	printf("\nrand_dir - %d",rand_dir);
+
 	printf("\n");
 	for(i=0;i<a;i++){
 		printf("%d ",edge[i]);
 	}
 	printf("\n%d - n\n",n);
-
-
+	
+	free(edge);
 }
 
